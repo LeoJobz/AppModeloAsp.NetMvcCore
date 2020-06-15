0b9a5eda7f562c345dc500be72f67c705e649391
Leonardo Santos <leonardo.cienciacomputer@gmail.com>
2020-06-15 18:16:17 -0300

    Add Produtos area

--- src/DevIOUI.Site/Startup.cs
+++ src/DevIOUI.Site/Startup.cs
@@ -31,10 +31,13 @@ namespace DevIOUI.Site
 
             app.UseMvc(routes =>
             {
-                routes.MapRoute(
-                        name: "default",
-                        template: "{controller=Home}/{action=Index}/{id?}");
-                //routes.MapRoute("default","{controller = Home}/{ action = Index}/{ id ?}");
+                //routes.MapRoute(
+                //        name: "default",
+                //        template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(name: "areas", template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
