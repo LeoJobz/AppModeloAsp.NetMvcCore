802560f0fde0c02ef10003f1b585b92b26ddfb6d
Leonardo Santos <leonardo.cienciacomputer@gmail.com>
2020-06-15 19:50:28 -0300

    Applying Dependency Injection

--- src/DevIOUI.Site/Controllers/HomeController.cs
+++ src/DevIOUI.Site/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static DevIOUI.Site.Data.PedidoRepository;
 
 namespace DevIOUI.Site.Controllers
 {
     public class HomeController : Controller
     {
+        //    private readonly IPedidoRepository _pedidoRepository;
 
-        public IActionResult Index()
+        //    public HomeController(IPedidoRepository pedidoRepository)
+        //    {
+        //        _pedidoRepository = pedidoRepository;
+        //    }
+
+        public IActionResult Index([FromServices] IPedidoRepository _pedidoRepository)
         {
+            var pedido = _pedidoRepository.ObterPedido();
+
             return View();
         }
     }
