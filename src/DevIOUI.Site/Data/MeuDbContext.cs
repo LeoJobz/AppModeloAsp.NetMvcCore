a8f815a91a72f767feff8382cc56cfbc06d9e4db
Leonardo Santos <leonardo.cienciacomputer@gmail.com>
2020-06-16 01:35:37 -0300

    DbContext config

---
+++ src/DevIOUI.Site/Data/MeuDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DevIOUI.Site.Data
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options) : base()
+        {
+        }
+    }
+}
--- src/DevIOUI.Site/Startup.cs
+++ src/DevIOUI.Site/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using static DevIOUI.Site.Data.PedidoRepository;
 
@@ -17,6 +19,14 @@ namespace DevIOUI.Site
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             //services.Configure<RazorViewEngineOptions>(options =>
@@ -27,6 +37,10 @@ namespace DevIOUI.Site
             //    options.AreaPageViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
             //    options.AreaPageViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
             //});
+
+
+            services.AddDbContext<MeuDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString(name: "MeuDbContext")));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
